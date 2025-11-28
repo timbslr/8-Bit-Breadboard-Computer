063674de4c2484b50625f702de0d0229197ef765
timbslr <94389494+timbslr@users.noreply.github.com>
2025-11-28 22:19:15 +0100

    adjusted 7-Segment Display decoder to have an unsigned instead of a hex mode

--- Control EEPROM Binary File Generators/7segDisplayGenerator.cpp
+++ Control EEPROM Binary File Generators/7segDisplayGenerator.cpp
@@ -20,7 +20,7 @@ const uint8_t dec_digits[] = {
   0b01111011, // 9
 };
 
-const uint8_t hex_digits[] = {
+/* const uint8_t hex_digits[] = {
   dec_digits[0], // 0
   dec_digits[1], // 1
   dec_digits[2], // 2
@@ -37,7 +37,7 @@ const uint8_t hex_digits[] = {
   0b00111101,    // D
   0b01001111,    // E
   0b01000111,    // F
-};
+}; */
 
 int computeValue(int mode, int digit, int num);
 
@@ -48,11 +48,11 @@ int main() {
   // 10 9 8 7 6 5 4 3 2 1 0
   //  M D D b b b b b b b b
   // msb                 lsb
-  // M = Mode(0 = signed, 1 = hexadecimal)
+  // M = Mode(0 = signed, 1 = unsigned)
   // D = Digit(00 = first, 01 = second, 10 = third, 11 = fourth)
   // b = Binary data from the bus(-128 to 127)
 
-  for (int mode = 0; mode <= 1; mode++) { // Mode : 0 = signed, 1 = hexadecimal
+  for (int mode = 0; mode <= 1; mode++) { // Mode : 0 = signed, 1 = unsigned
     for (int digit = 0; digit <= 3; digit++) {
       for (int num = -128; num <= 127; num++) {
         int address = (mode << 10) | (digit << 8) | (uint8_t)num;
@@ -73,24 +73,26 @@ int main() {
 // D a b c d e f g
 // where D is the decimal point and a - g are the segments of the 7 - segment display
 int computeValue(int mode, int digit, int num) {
-  if (mode == 0) { // signed decimal
-    std::string numString = std::to_string(num);
-
-    while (numString.size() < 4) {
-      numString = '\0' + numString;
-    }
-
-    char char_to_print = numString[digit];
-
-    if (char_to_print == '\0') return 0b00000000;
-    else if (char_to_print == '-') return 0b00000001;
-    return dec_digits[char_to_print - '0'];
+  if(mode == 1 && num < 0) {
+    num += 256; //-1 --> 255, -2 --> 254, ...
   }
-  else {   // hex
-    int indices[] = { 0, 0, ((uint8_t)num) >> 4, num & 0x0F };
+  std::string numString = std::to_string(num);
+  
+  while (numString.size() < 4) {
+    numString = '\0' + numString;
+  }
+  
+  char char_to_print = numString[digit];
+
+  if (char_to_print == '\0') return 0b00000000;
+  else if (char_to_print == '-') return 0b00000001;
+  return dec_digits[char_to_print - '0'];
+
+
+  /* int indices[] = { 0, 0, ((uint8_t)num) >> 4, num & 0x0F };   //hex mode
     int index = indices[digit];
 
     if (index == 0 && digit != 0b11) return 0b00000000;
-    return hex_digits[index];
-  }
+    return hex_digits[index]; */
+  
 }
--- asm/examples/fibonacci_iterative.asm
+++ asm/examples/fibonacci_iterative.asm
@@ -1,11 +1,12 @@
 #include "rules.asm"
 
+s7sdum ; set 7-Segment display unsigned mode
 li A, 1
 li B, 1
 
 fib:
-  bvs end    ; branch if next fib number > 127
-  out7sd B   ; The last output will be 89 as the next fibonacci number (144) is greater than the maximum number that can be displayed (127)
+  bcs end    ; branch if next fib number > 255
+  out7sd B   ; The last output will be 233 as the next fibonacci number (377) is greater than the maximum number that can be displayed (255)
   mov TMP, B ; TMP := B
   add B      ; B = A + TMP
   mov A, TMP ; A := TMP
