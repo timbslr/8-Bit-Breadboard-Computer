69497d5dbde8147ccc78ebfdeb7b7df1204b8c20
timbslr <94389494+timbslr@users.noreply.github.com>
2025-12-11 16:38:08 +0100

    docs: improved loading time of complete Parts List

--- docs/src/MapExtension.js
+++ docs/src/MapExtension.js
@@ -1,9 +1,8 @@
 Map.prototype.concatBySum = function (map2) {
-  const mergedMap = new Map(this);
   for (const [key, intValue] of map2) {
     const previousInt = this.get(key) || 0;
-    mergedMap.set(key, previousInt + intValue);
+    this.set(key, previousInt + intValue);
   }
 
-  return mergedMap;
+  return this;
 };
