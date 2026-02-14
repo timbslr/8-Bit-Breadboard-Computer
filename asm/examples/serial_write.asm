b5f16b58e7fc7b1e88d83efe6f5b02650a1880eb
timbslr <94389494+timbslr@users.noreply.github.com>
2026-02-14 12:44:45 +0100

    made the Serial Transmitter work at 1Mhz

--- asm/examples/serial_write.asm
+++ asm/examples/serial_write.asm
@@ -3,12 +3,12 @@
 STRING_ADDR = 0x6000
 
 #addr STRING_ADDR
-#d "Hey, it's working!\nFull speed incoming!!!\nLets start some Wozmon Action in\n3\n2\n1\nWOZMON!!! --------------------GOGOGO----------------" ; The string that should be sent via serial
+#d "Hello World!\r\nIt finally works!\r\n3\r\n2\r\n1\r\n---GOGOGO---\r\n" ; The string that should be sent via serial
 #addr 0
 
 loop:
   mov A, X
-  bgei 120, end; bgeiu broken?
+  bgei 56, end; bgeiu broken? //TODO fix
   ldo B, STRING_ADDR
 rdy_wait:
   btxrdyc rdy_wait
